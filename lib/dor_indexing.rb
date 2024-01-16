3fdd799f5699ba7046953b404148f84c32da10ce
Justin Littman <justinlittman@stanford.edu>
2024-01-16 08:43:39 -0500

    Adds code comment

--- lib/dor_indexing.rb
+++ lib/dor_indexing.rb
@@ -11,6 +11,7 @@ Zeitwerk::Loader.for_gem.setup
 
 # Builds solr documents for indexing.
 class DorIndexing
+  # @return [Hash] the solr document
   def self.build(cocina_with_metadata:, workflow_client:, cocina_repository:)
     Honeybadger.context({ identifier: cocina_with_metadata.externalIdentifier })
     DorIndexing::Builders::DocumentBuilder.for(model: cocina_with_metadata, workflow_client:, cocina_repository:).to_solr
