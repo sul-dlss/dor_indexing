5dd312d69da157956a855aa6ecebbf124a8482ac
Naomi Dushay <ndushay@stanford.edu>
2024-02-15 12:13:35 -0800

    workflow indexer classes get better comments and names

--- lib/dor_indexing/indexers/workflows_indexer.rb
+++ lib/dor_indexing/indexers/workflows_indexer.rb
@@ -2,7 +2,7 @@
 
 class DorIndexing
   module Indexers
-    # Indexes the objects position in workflows
+    # Indexes the object's state in the most recent execution of every one of its workflows
     class WorkflowsIndexer
       attr_reader :id
 
@@ -11,7 +11,7 @@ class DorIndexing
         @workflow_client = workflow_client
       end
 
-      # @return [Hash] the partial solr document for workflow concerns
+      # @return [Hash] the partial solr document for workflows concerns
       def to_solr
         WorkflowSolrDocument.new do |combined_doc|
           workflows.each do |wf|
@@ -30,7 +30,6 @@ class DorIndexing
         all_workflows.workflows
       end
 
-      # TODO: remove Dor::Workflow::Document
       # @return [Workflow::Response::Workflows]
       def all_workflows
         @all_workflows ||= workflow_client.workflow_routes.all_workflows pid: id
